08b3189d1c7448b89477aa05c135b95c99279b17
Kazu Yamamoto <kazu@iij.ad.jp>
2012-06-19 23:38:25 +0900

    cosmetic change.

--- test/SendfileSpec.hs
+++ test/SendfileSpec.hs
@@ -22,24 +22,24 @@ spec = do
             runSendfile (PartOfFile 2000 1000000) `shouldReturn` ExitSuccess
 
 runSendfile :: FileRange -> IO ExitCode
-runSendfile range = bracket setup teardown body
+runSendfile range = bracket setup teardown $ \(s2,_) -> do
+    runResourceT $ sourceSocket s2 $$ sinkFile outputFile
+    runResourceT $ copyfile range
+    system $ "cmp -s " ++ outputFile ++ " " ++ expectedFile
   where
+    copyfile EntireFile = sourceFile inputFile $$ sinkFile expectedFile
+    copyfile (PartOfFile off len) =
+        sourceFile inputFile $= CB.isolate (off' + len') $$
+            (CB.take off' >> sinkFile expectedFile)
+      where
+        off' = fromIntegral off
+        len' = fromIntegral len
     setup = do
         (s1,s2) <- socketPair AF_UNIX Stream 0
         tid <- forkIO (sendfile s1 inputFile range (return ()) `finally` sendEOF s1)
         return (s2,tid)
       where
         sendEOF = sClose
-    body (s2,_) = do
-        runResourceT $ sourceSocket s2 $$ sinkFile outputFile
-        let run = case range of
-                EntireFile -> sourceFile inputFile $$ sinkFile expectedFile
-                PartOfFile start end -> do
-                    let start' = fromIntegral start
-                        end' = fromIntegral end
-                    sourceFile inputFile $= CB.isolate (start' + end') $$ (CB.take start' >> sinkFile expectedFile)
-        runResourceT run
-        system $ "cmp -s " ++ outputFile ++ " " ++ expectedFile
     teardown (s2,tid) = do
         sClose s2
         killThread tid
